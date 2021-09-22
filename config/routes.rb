f91d7b21940725d310bec7d6ade0a8b99d29bdd9
mrabets <newrabgo@gmail.com>
2021-09-22 15:28:05 +0300

    Make simple user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   root to: 'pages#home'
-  
+
   get 'admin' => 'admin#home'
+
+  resources :users, :only =>[:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
