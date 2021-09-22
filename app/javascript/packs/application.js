aabe41ac5fabb9a22b9ae2e8c24e8b29cd6377cd
mrabets <newrabgo@gmail.com>
2021-09-22 13:48:41 +0300

    Add disappearing toastr messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'stylesheets/application'
 
+global.toastr = require("toastr")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
