c647bad97cfe698a2f190305bac0bfefa8363eae
mrabets <newrabgo@gmail.com>
2021-09-22 17:51:40 +0300

    Fix current_user path

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  get '/users/:id', to: 'users#show'
 
+    resources :users, :only =>[:show]
   scope "(:locale)", locale: /en|ru/ do
-    devise_for :users, skip: :omniauth_callbacks
+    devise_for :users, :path_prefix => 'd', skip: :omniauth_callbacks
 
-    get 'users/show'
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     
     root to: 'pages#home'
 
     get 'admin' => 'admin#home'
 
-    resources :users, :only =>[:show]
+    
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
