8a3236a0ff6eea8241c458f501010fb4a94d6417
mrabets <newrabgo@gmail.com>
2021-09-24 15:25:56 +0300

    Improve profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
+    
+    if @user.image.nil?
+      @user.image = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" 
+    end
+
+    if @user.name.nil?
+      @user.name = "User-#{@user.id}"
+    end
   end
 end
