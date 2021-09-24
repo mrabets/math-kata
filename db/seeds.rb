b1caee33e2e75cc4e9e6893c308119204d986257
mrabets <newrabgo@gmail.com>
2021-09-24 23:16:11 +0300

    Generate subject model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Subject.create! name: 'Geometry'
+Subject.create! name: 'Algebra'
+Subject.create! name: 'Arithmetic'
+Subject.create! name: 'Trigonometry'
+Subject.create! name: 'Calculus'
+Subject.create! name: 'Logic'
+Subject.create! name: 'Number theory'
+Subject.create! name: 'Java'
+Subject.create! name: 'Python'
+Subject.create! name: 'C/C++'
+Subject.create! name: 'Python'
+Subject.create! name: 'Ruby'
