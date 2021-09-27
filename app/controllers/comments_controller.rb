b453eb312916be94168e7e48f9a84a505d6bccd6
mrabets <newrabgo@gmail.com>
2021-09-27 15:03:49 +0300

    Add user name to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
   def create
     @task = Task.find(params[:task_id])
-    @comment = @task.comments.create(params[:comment].permit(:message))
-    redirect_to @task   
+    @comment = @task.comments.new comment_params
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to @task       
   end
 
   def destroy
 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
   end
 end
