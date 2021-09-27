2eeef60324686efdb1709420de06ae01ab65941f
mrabets <newrabgo@gmail.com>
2021-09-27 13:09:55 +0300

    Add task id to comments

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :answers, answers: true 
 
   belongs_to :user
+  has_many   :comments
   has_many   :tags
   accepts_nested_attributes_for :tags
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_200820) do
+ActiveRecord::Schema.define(version: 2021_09_27_100552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_09_24_200820) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "task_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
