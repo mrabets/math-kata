79dda3a4e9da8cb0de4a47faa86b197aba9b9296
mrabets <newrabgo@gmail.com>
2021-10-04 20:18:58 +0300

    Add unlike and undislike withour ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :find_task
+  before_action :find_comment, only: [:like, :unlike, :dislike, :undislike]
+
   def create
-    @task = Task.find(params[:task_id])
-    @comment = @task.comments.new comment_params
     @comment.user_id = current_user.id
     @comment.save
     redirect_to @task       
   end
 
   def like
-    @task = Task.find(params[:task_id])
-    @comment = Comment.find params[:id]
     @comment.liked_by current_user
     redirect_to @task
   end
 
+  def unlike
+    @comment.unliked_by current_user
+    redirect_to @task
+  end
+
   def dislike
-    @task = Task.find(params[:task_id])
-    @comment = Comment.find params[:id]
     @comment.disliked_by current_user
+    redirect_to @task
+  end
+
+  def undislike
+    @comment.undisliked_by current_user
     redirect_to @task
   end
 
@@ -26,6 +33,14 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def find_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def find_comment
+    @comment = Comment.find params[:id]
+  end
 
   def comment_params
     params.require(:comment).permit(:message)
