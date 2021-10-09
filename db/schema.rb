55e76b56098437d9f617db6c29d3470db4e0f86f
mrabets <newrabgo@gmail.com>
2021-10-09 15:57:00 +0300

    Remove unnecessary condition column from tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_093354) do
+ActiveRecord::Schema.define(version: 2021_10_09_125400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +110,6 @@ ActiveRecord::Schema.define(version: 2021_10_06_093354) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "condition"
     t.text "answers"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
