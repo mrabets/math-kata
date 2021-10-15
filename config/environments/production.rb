d5ad2122daba1868d93579a918b857246607e117
mrabets <newrabgo@gmail.com>
2021-10-15 15:42:49 +0300

    Install redis production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://math-kata.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://math-kata.herokuapp.com', /http:\/\/math-kata.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
