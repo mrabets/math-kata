8d9ef515a89f88f79997718080c91742a9ef42d8
mrabets <newrabgo@gmail.com>
2021-10-16 11:29:16 +0300

    Fix fitures

--- db/schema.rb
+++ db/schema.rb
@@ -10,172 +10,170 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_011_102_446) do
+ActiveRecord::Schema.define(version: 2021_10_11_102446) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'action_text_rich_texts', force: :cascade do |t|
-    t.string 'name', null: false
-    t.text 'body'
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.text 'message'
-    t.integer 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'task_id'
-    t.integer 'cached_scoped_subscribe_votes_total', default: 0
-    t.integer 'cached_scoped_subscribe_votes_score', default: 0
-    t.integer 'cached_scoped_subscribe_votes_up', default: 0
-    t.integer 'cached_scoped_subscribe_votes_down', default: 0
-    t.integer 'cached_weighted_subscribe_score', default: 0
-    t.integer 'cached_weighted_subscribe_total', default: 0
-    t.float 'cached_weighted_subscribe_average', default: 0.0
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "task_id"
+    t.integer "cached_scoped_subscribe_votes_total", default: 0
+    t.integer "cached_scoped_subscribe_votes_score", default: 0
+    t.integer "cached_scoped_subscribe_votes_up", default: 0
+    t.integer "cached_scoped_subscribe_votes_down", default: 0
+    t.integer "cached_weighted_subscribe_score", default: 0
+    t.integer "cached_weighted_subscribe_total", default: 0
+    t.float "cached_weighted_subscribe_average", default: 0.0
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'pg_search_documents', force: :cascade do |t|
-    t.text 'content'
-    t.string 'searchable_type'
-    t.bigint 'searchable_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[searchable_type searchable_id], name: 'index_pg_search_documents_on_searchable'
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
-  create_table 'ratings', force: :cascade do |t|
-    t.integer 'score'
-    t.integer 'user_id'
-    t.integer 'task_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "ratings", force: :cascade do |t|
+    t.integer "score"
+    t.integer "user_id"
+    t.integer "task_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'solved_tasks', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'task_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "solved_tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "task_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'subjects', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_subjects_on_name'
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_subjects_on_name"
   end
 
-  create_table 'taggings', id: :serial, force: :cascade do |t|
-    t.integer 'tag_id'
-    t.string 'taggable_type'
-    t.integer 'taggable_id'
-    t.string 'tagger_type'
-    t.integer 'tagger_id'
-    t.string 'context', limit: 128
-    t.datetime 'created_at'
-    t.string 'tenant', limit: 128
-    t.index ['context'], name: 'index_taggings_on_context'
-    t.index %w[tag_id taggable_id taggable_type context tagger_id tagger_type], name: 'taggings_idx',
-                                                                                unique: true
-    t.index ['tag_id'], name: 'index_taggings_on_tag_id'
-    t.index %w[taggable_id taggable_type context], name: 'taggings_taggable_context_idx'
-    t.index %w[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
-    t.index ['taggable_id'], name: 'index_taggings_on_taggable_id'
-    t.index ['taggable_type'], name: 'index_taggings_on_taggable_type'
-    t.index %w[tagger_id tagger_type], name: 'index_taggings_on_tagger_id_and_tagger_type'
-    t.index ['tagger_id'], name: 'index_taggings_on_tagger_id'
-    t.index ['tenant'], name: 'index_taggings_on_tenant'
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.string "tenant", limit: 128
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    t.index ["tenant"], name: "index_taggings_on_tenant"
   end
 
-  create_table 'tags', id: :serial, force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'taggings_count', default: 0
-    t.index ['name'], name: 'index_tags_on_name', unique: true
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'title'
-    t.text 'answers'
-    t.integer 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'subject'
-    t.index ['user_id'], name: 'index_tasks_on_user_id'
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "answers"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "subject"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'name'
-    t.text 'image'
-    t.boolean 'superadmin_role', default: false
-    t.boolean 'user_role', default: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
+    t.boolean "superadmin_role", default: false
+    t.boolean "user_role", default: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'votes', force: :cascade do |t|
-    t.string 'votable_type'
-    t.bigint 'votable_id'
-    t.string 'voter_type'
-    t.bigint 'voter_id'
-    t.boolean 'vote_flag'
-    t.string 'vote_scope'
-    t.integer 'vote_weight'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[votable_id votable_type vote_scope],
-            name: 'index_votes_on_votable_id_and_votable_type_and_vote_scope'
-    t.index %w[votable_type votable_id], name: 'index_votes_on_votable_type_and_votable_id'
-    t.index %w[voter_id voter_type vote_scope], name: 'index_votes_on_voter_id_and_voter_type_and_vote_scope'
-    t.index %w[voter_type voter_id], name: 'index_votes_on_voter_type_and_voter_id'
+  create_table "votes", force: :cascade do |t|
+    t.string "votable_type"
+    t.bigint "votable_id"
+    t.string "voter_type"
+    t.bigint "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'taggings', 'tags'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "taggings", "tags"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Subject.create! name: 'Geometry'
-Subject.create! name: 'Algebra'
-Subject.create! name: 'Arithmetic'
-Subject.create! name: 'Trigonometry'
-Subject.create! name: 'Calculus'
-Subject.create! name: 'Logic'
-Subject.create! name: 'Number theory'
-Subject.create! name: 'Java'
-Subject.create! name: 'Python'
-Subject.create! name: 'C/C++'
-Subject.create! name: 'Python'
-Subject.create! name: 'Ruby'
+user = User.create!({ 
+  name: 'Admin',
+  email: 'admin@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  superadmin_role: true,
+})
+
+Subject.create!([{
+  name: 'Geometry'
+},
+{
+  name: 'Algebra'
+},
+{
+  name: 'Arithmetic'
+},
+{
+  name: 'Trigonometry'
+},
+{
+  name: 'Calculus'
+},
+{
+  name: 'Logic'
+},
+{
+  name: 'Number theory'
+},
+{
+  name: 'Computer Science'
+},
+{
+  name: 'Cryptography'
+},
+{
+  name: 'Probability theory'
+},
+]) 
+
+task = Task.new(
+  title: 'The perimeter of triangle',
+  answers: '20;20.0',
+  user_id: user.id,
+  subject: 'Geometry',
+  condition: '<div class="trix-content"> <div>A triangle has a <em>perimeter</em> of 50. If 2 of its sides are <strong>equal</strong> and the third side is 5 more than the equal sides.<br><br> </div><div><action-text-attachment content-type="image" url="https://www.onlinemathlearning.com/image-files/geometry-problem-triangle-1.gif" width="168" height="111" caption="Perimeter of triangle"><figure class="attachment attachment--preview"> <img width="168" height="111" src="https://www.onlinemathlearning.com/image-files/geometry-problem-triangle-1.gif"> <figcaption class="attachment__caption"> Perimeter of triangle </figcaption> </figure></action-text-attachment></div><div> <br>What is the length of the third <strong><em>side</em></strong>?</div> </div>'
+)
+task.tag_list.add(['basic', 'figures'])
+task.save
+
+task = Task.new(
+  title: 'Solve the equation',
+  answers: '0=0;0.0=0.0',
+  user_id: user.id,
+  subject: 'Algebra',
+  condition: '<div class="trix-content"> <div>Given the equation:<br><br> </div><blockquote><em>5 (- 3 x - 2) - (x - 3) = - 4 (4 x + 5) + 13</em></blockquote> </div>'
+)
+task.tag_list.add(['code', 'basic'])
+task.save
+
+task = Task.new(
+  title: 'The Birch and Swinnerton-Dyer Conjecture',
+  answers: '1',
+  user_id: user.id,
+  subject: 'Calculus',
+  condition: '<div class="trix-content"> <div> <action-text-attachment content-type="image" url="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/436px-bsd-data-plot-for-elliptic-curve-800h1-svg-1569527124.png?crop=1xw:1xh;center,top&amp;resize=480:*" width="436" height="296"><figure class="attachment attachment--preview"> <img width="436" height="296" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/436px-bsd-data-plot-for-elliptic-curve-800h1-svg-1569527124.png?crop=1xw:1xh;center,top&amp;resize=480:*"> </figure></action-text-attachment><br>The <a href="https://www.claymath.org/millennium-problems/birch-and-swinnerton-dyer-conjecture">Birch and Swinnerton-Dyer Conjecture</a> is another of the six unsolved Millennium Prize Problems, and it\'s the only other one we can remotely describe in plain English. This Conjecture involves the math topic known as Elliptic Curves.<br><br> </div><div>When we recently <a href="https://www.popularmechanics.com/science/math/g29008356/hard-math-problems/">wrote about the toughest math problems that have been solved</a>, we mentioned one of the greatest achievements in 20th-century math: the solution to Fermat\'s Last Theorem. Sir Andrew Wiles solved it using Elliptic Curves. So, you could call this a very powerful new branch of math.<br><br> </div><div>In a nutshell, an elliptic curve is a special kind of function. They take the unthreatening-looking form y²=x³+ax+b. It turns out functions like this have certain properties that cast insight into math topics like Algebra and Number Theory.</div><div> <br>British mathematicians Bryan Birch and Peter Swinnerton-Dyer developed their conjecture in the 1960s. Its exact statement is very technical, and has evolved over the years. One of the main stewards of this evolution has been none other than Wiles. To see its current status and complexity, check out <a href="http://www.claymath.org/sites/default/files/birchswin.pdf">this famous update</a> by Wells in 2006.<br><br><br> </div> </div>'
+)
+task.tag_list.add(['high level', 'hard', 'code'])
+task.save
+
+
+
+
